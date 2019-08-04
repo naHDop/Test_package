6e12bbad538152cf5127fbed6e9477eb74d34377
Grigory <tarasov198726@gmail.com>
2019-08-04 16:22:21 +0300

    add Order

--- controllers/shop.js
+++ controllers/shop.js
@@ -122,21 +122,28 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
   .getCart()
   .then(cart => {
+    fetchedCart = cart;
     return cart.getProducts();
   })
   .then(products => {
     return req.user
     .createOrder()
     .then(order => {
-      return order.addProducts(products.map(product => {
-        product.orderItem = { quantity: product.cartItem.quantity };
-        return product;
-      }))
+      return order.addProducts(
+        products.map(product => {
+          product.orderItem = { quantity: product.cartItem.quantity };
+          return product;
+        })
+      );
     })
     .catch(err => { console.log(err) });
+  })
+  .then(result => {
+    return fetchedCart.setProducts(null);
   })
   .then(result => {
     res.redirect('/orders');
@@ -145,15 +152,14 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+  req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => { console.log(err) })
 };
